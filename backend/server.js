dd6dc6aee5148c35d332848e0cecc779f0ea8d49
alfarahas <alfarahas2001@gmail.com>
2025-10-16 13:52:42 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from React build (for production)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 }
 
 // Routes
@@ -102,7 +102,7 @@ if (process.env.NODE_ENV === 'production') {
     }
     
     // Otherwise, serve the React app
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   });
 }
 
